437286749b87b773153f294ed345d16f3cd85fa0
tianmin <tianmin@pku.edu.cn>
2020-02-13 16:43:09 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="min_div">
+      <life-and-death-tencent/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LifeAndDeathTencent from "./components/LifeAndDeathTencent"
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    LifeAndDeathTencent
+  },
+  mounted: function() {
   }
-}
+};
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+}
+
+#min_div {
+  // position: absolute;
+  left: 0;
+  right: 0;
+  // width: 100%;
+  // height: 100%;
+  // border: 1px solid red;
+  text-align: center;
+  // height: 100%;
+  overflow: hidden;
 }
 </style>
